c193f10845f72843c5a16761f3cc51459eec5279
shabie <30535146+shabie@users.noreply.github.com>
2020-06-27 23:29:16 +0200

    update redeploy.sh to fix namespaces from default to my own

--- redeploy.sh
+++ redeploy.sh
@@ -4,5 +4,5 @@ echo "redeploy.sh executed"
 
 docker stop auto-flask
 docker rm auto-flask
-docker pull pythonincontainers/auto-flask:latest
-docker run -d --name auto-flask -p 5000:5000 pythonincontainers/auto-flask:latest
+docker pull shabie/auto-flask:latest
+docker run -d --name auto-flask -p 5000:5000 shabie/auto-flask:latest
